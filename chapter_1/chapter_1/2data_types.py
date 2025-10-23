d4abe7633ab5f6ae566961863bb3559973a55080
ahmii945 <ahmedarain717@gmail.com>
2025-10-23 15:10:30 +0500

    Add files via upload
    Added Chapter 1 practice Python files

---
+++ chapter_1/chapter_1/2data_types.py
@@ -0,0 +1,5 @@
+a = 1 # a is an integer
+b = 5.45 # b is a floating point number
+c = "ahmed" # c is a string
+d = False # d is an boolean varaible
+e = None # e is a none type varaible
---
+++ chapter_1/chapter_1/3rules_variables.py
@@ -0,0 +1,10 @@
+a = 24
+aaa = 486
+harry = 34
+sameer = 56
+
+__sameerr = 34
+
+@sameer #inavlid due to @ symbol
+
+sameer = 52 #invalid due to digit in starting
