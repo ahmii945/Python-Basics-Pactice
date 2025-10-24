64d2b300a58ea49b499a0748c068a06e3dc28c2d
ahmii945 <ahmedarain717@gmail.com>
2025-10-24 23:01:58 +0500

    Update and rename 02_negative_slicing.py to chapter_2/slicing.py

--- chapter_2/slicing.py
+++ chapter_2/slicing.py
@@ -1,4 +1,4 @@
-name = "Harry"
+name = "AHMED"
 
 print(name[0:3])
 
@@ -7,4 +7,5 @@ print(name[1:4])
 
 print(name[:4]) # is same as print(name[0:4])
 print(name[1:]) # is same as print(name[1:5])
+
 print(name[1:5])
